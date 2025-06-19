7c376c6263a0e629a8abeb1eca82daae0afb03f2
Chien Kuo <kenkuo860129@gmail.com>
2025-06-19 10:06:44 +0000

    Add seed and is_valid to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -2,4 +2,31 @@
     config(materialized='table')
 }}
 
-SELECT * FROM {{source('jaffle_shop', 'orders')}}
+WITH source AS (
+    SELECT * FROM {{source('jaffle_shop', 'orders')}}
+),
+
+status_mapping AS (
+    SELECT * FROM {{ref('seed_order_statuses')}}
+),
+
+transformed AS (
+    SELECT
+        s.id,
+        s.user_id,
+        s.order_date,
+        s.status,
+        s._etl_loaded_at,
+        m.is_valid
+    FROM
+        source AS s
+    LEFT JOIN 
+        status_mapping AS m
+    ON 
+        s.status = m.status
+)
+
+SELECT
+    *
+FROM 
+    transformed
